40d1d9ee6b8137a2944c1455213dad13fdc1217a
Cameron Yick <cameron.yick@gmail.com>
2020-09-09 22:51:05 +0000

    PRF: deRemove unused files

--- streamlit-vega-lite/frontend/src/vega-loader-arrow.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'vega-loader-arrow' {
-  import type { Table } from "apache-arrow"
-
-  // Convert arrow table to an array of proxy objects
-  function arrow(data: Table | ArrayBuffer | Object[]): Object[];
-
-  export default arrow;
-}
