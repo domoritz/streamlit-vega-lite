4a6816d987f2dfeb60a7f44f9460eee98ca58202
Dominik Moritz <domoritz@gmail.com>
2020-09-10 03:40:57 +0000

    update authors

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setuptools
 setuptools.setup(
     name="streamlit-vega-lite",
     version="0.0.1",
-    author="Dominik Moritz",
+    author="Dominik Moritz, Cameron Yick",
     author_email="",
-    description="",
-    long_description="A Vega-Lite Component for Streamlit that Supports Selections",
+    description="A Vega-Lite Component for Streamlit that Supports Selections",
+    long_description="",
     long_description_content_type="text/plain",
     url="",
     packages=setuptools.find_packages(),
@@ -14,9 +14,6 @@ setuptools.setup(
     classifiers=[],
     python_requires=">=3.6",
     install_requires=[
-        # By definition, a Custom Component depends on Streamlit.
-        # If your component has other Python dependencies, list
-        # them here.
         "streamlit >= 0.63",
     ],
 )
